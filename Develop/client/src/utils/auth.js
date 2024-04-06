57a76ff78f3557373bd0cd9f7ca3c0a74a027f1e
Lillian Vanderbloemen <lillvander18@gmail.com>
2024-04-06 16:12:50 -0600

    updated logic in auth.js file to handle logins and add user

--- Develop/client/src/utils/auth.js
+++ Develop/client/src/utils/auth.js
@@ -1,6 +1,11 @@
 // use this to decode a token and get the user's information out of it
 import decode from 'jwt-decode';
 
+// Lily's changes 
+import { userMutation } from '@apollo/client' //import apollo
+import { LOGIN_USER, ADD_USER } from '../utils/mutations'; // Import GraphQL mutations
+
+
 // create a new class to instantiate for a user
 class AuthService {
   // get user data
@@ -32,11 +37,41 @@ class AuthService {
     return localStorage.getItem('id_token');
   }
 
-  login(idToken) {
-    // Saves user token to localStorage
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
+  // Lily's changes | handle user logins:
+  async login(email, password) {
+    try {
+      const [login] = userMutation(LOGIN_USER);
+      const {data} = await login({
+        variables: {email, password}
+      });
+      const { token } = data.login;
+      localStorage.setItem('id_token', token);
+      window.location.assign('/');   
+    } catch (err) {
+      console.log(err)
+    }
   }
+
+  // Lily's changes | Handle user sign up 
+  async signup(username, email, password) {
+    try {
+      const [addUser] = useMutation(ADD_USER);
+      const { data } = await addUser({
+        variables: { username, email, password }
+      });
+      const { token } = data.addUser;
+      localStorage.setItem('id_token', token);
+      window.location.assign('/');
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  //login(idToken) {
+    // Saves user token to localStorage
+   // localStorage.setItem('id_token', idToken);
+   // window.location.assign('/');
+ // }
 
   logout() {
     // Clear user token and profile data from localStorage
